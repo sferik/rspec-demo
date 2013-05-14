c0ab9755a54e48f90baa6890b010fbffa5a50a6d
Erik Michaels-Ober <sferik@gmail.com>
2013-05-14 14:26:56 -0700

    Add failing test for Course#students

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -7,4 +7,14 @@ describe Course do
     expect(course.name).to eq "WDI"
   end
 
+  it "has many students" do
+    course = Course.create(:name => "WDI")
+    student1 = Student.create(:course_id => course.id)
+    student2 = Student.create(:course_id => course.id)
+    expect(course.students).to be_an Array
+    expect(course.students.size).to eq 2
+    expect(course.students).to include student1
+    expect(course.students).to include student2
+  end
+
 end
