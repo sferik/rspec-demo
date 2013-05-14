c62a4b17a8b1e6f3f1bbd83984cd3cc23338e416
Erik Michaels-Ober <sferik@gmail.com>
2013-05-14 13:59:43 -0700

    Make spec pass by generating Course model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130514204338) do
+ActiveRecord::Schema.define(:version => 20130514205830) do
+
+  create_table "courses", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "students", :force => true do |t|
     t.string   "name"
